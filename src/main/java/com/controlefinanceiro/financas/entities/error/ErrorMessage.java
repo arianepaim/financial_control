7bc9f1f6098364e54569af5f6ab60ca162a11232
Ariane Paim <arianeepaimm@gmail.com>
2023-11-01 16:51:19 -0300

    enhanced error handling for missing resources

---
+++ src/main/java/com/controlefinanceiro/financas/entities/error/ErrorMessage.java
@@ -0,0 +1,21 @@
+package com.controlefinanceiro.financas.entities.error;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ErrorMessage {
+
+    private Date dateNow;
+    private String title;
+    private int status;
+    private String message;
+
+    public ErrorMessage(String title, int status, String message) {
+        this.dateNow = new Date();
+        this.title = title;
+        this.status = status;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/controlefinanceiro/financas/handler/RestExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.controlefinanceiro.financas.handler;
+
+import com.controlefinanceiro.financas.entities.error.ErrorMessage;
+import com.controlefinanceiro.financas.entities.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException e) {
+
+        ErrorMessage message = new ErrorMessage("Not found.", HttpStatus.NOT_FOUND.value(), e.getMessage());
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+}
