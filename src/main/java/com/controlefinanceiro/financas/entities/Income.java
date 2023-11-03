1d211d65c8b8215344d634236a4eea9bc8d74bd7
Ariane Paim <arianeepaimm@gmail.com>
2023-11-03 11:15:16 -0300

    entity income

---
+++ src/main/java/com/controlefinanceiro/financas/entities/Income.java
@@ -0,0 +1,38 @@
+package com.controlefinanceiro.financas.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "tb_income")
+public class Income {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String name;
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String category;
+    @NotNull
+    @NotBlank
+    private Date date;
+    @NotNull
+    private Double value;
+    @NotNull
+    private Long userId;
+}
