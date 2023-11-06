ab2485f6d79f61d0e9fb754dcb2bcebcea1c2b71
Ariane Paim <arianeepaimm@gmail.com>
2023-11-06 14:16:34 -0300

    adjustment in income classes

--- src/main/java/com/controlefinanceiro/financas/repositories/IncomeRepository.java
+++ src/main/java/com/controlefinanceiro/financas/repositories/IncomeRepository.java
@@ -4,6 +4,10 @@ import com.controlefinanceiro.financas.entities.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+
+    List<Income> findAllByUserId(Long userId);
 }
--- src/main/java/com/controlefinanceiro/financas/view/model/income/IncomeRequest.java
+++ src/main/java/com/controlefinanceiro/financas/view/model/income/IncomeRequest.java
@@ -1,5 +1,6 @@
 package com.controlefinanceiro.financas.view.model.income;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -8,7 +9,7 @@ import java.util.Date;
 public class IncomeRequest {
     private String name;
     private String category;
+    @JsonFormat(pattern = "dd/MM/yyyy", timezone = "America/Sao_Paulo")
     private Date date;
     private Double value;
-    private Long userId;
 }
